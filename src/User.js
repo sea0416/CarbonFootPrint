861e74cb763ed37c0d6d745f429ed69a6c485fd0
sea0416 <sea0416@naver.com>
2022-01-07 17:10:40 -0600

    Third commit (add map directions and the marker)

--- src/User.js
+++ src/User.js
@@ -1,14 +1,45 @@
-import * as React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import React, {useState} from 'react';
+import { View } from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
 
 export default function User() {
+    const [coordinates] = useState([
+        {
+          latitude: 48.8587741,
+          longitude: 2.2069771,
+        },
+        {
+          latitude: 48.8323785,
+          longitude: 2.3361663,
+        },
+    ]);
 
     return (
         <View>
             <MapView
                 style={{height: '50%', width: '100%'}}
-            />
+                provider={PROVIDER_GOOGLE}
+                showsUserLocation={true}
+                initialRegion={{
+                    latitude: coordinates[0].latitude,
+                    longitude: coordinates[0].longitude,
+                    latitudeDelta: 0.0622,
+                    longitudeDelta: 0.0121,
+                  }}
+            >
+            <MapViewDirections
+                origin={coordinates[0]}
+                destination={coordinates[1]}
+                apikey={"AIzaSyBrkYoHLtzAamkjzt89FaC69TopUxvSERA"} // insert your API Key here
+                strokeWidth={4}
+                strokeColor="#111111"
+                />
+            <Marker coordinate={coordinates[0]} />
+            <Marker coordinate={coordinates[1]} />
+            </MapView>
+
         </View>
     );
 }
